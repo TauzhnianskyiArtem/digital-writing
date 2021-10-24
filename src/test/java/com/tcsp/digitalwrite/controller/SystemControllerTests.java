2bf1c948d906c0c5a04a20e939f96425a021fbaf
Tauzhnyanskiy Artiom <art121bog@gmail.com>
2021-10-24 21:06:36 +0300

    Add test for delete system

--- src/test/java/com/tcsp/digitalwrite/controller/SystemControllerTests.java
+++ src/test/java/com/tcsp/digitalwrite/controller/SystemControllerTests.java
@@ -1,6 +1,10 @@
 package com.tcsp.digitalwrite.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tcsp.digitalwrite.api.controller.SystemController;
+import com.tcsp.digitalwrite.api.dto.SystemDto;
+import com.tcsp.digitalwrite.store.entity.SystemEntity;
+import com.tcsp.digitalwrite.store.repository.SystemRepository;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.junit.jupiter.api.Test;
@@ -9,8 +13,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,17 +30,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SystemControllerTests {
 
     @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    SystemRepository systemRepository;
+
+    @Autowired
     MockMvc mockMvc;
 
 
     @Test
     public void addSystem() throws Exception {
-        String nameSystem = "Test name system";
+        String nameSystem = "Test system";
 
-        mockMvc.perform(post(SystemController.CREATE_SYSTEM)
-                        .param("name", nameSystem))
-                        .andDo(MockMvcResultHandlers.print())
-                        .andExpect(status().isOk())
-                        .andReturn();
+        MvcResult result = mockMvc.perform(post(SystemController.CREATE_SYSTEM)
+                .param("name", nameSystem))
+                .andExpect(status().isOk())
+                .andReturn();
+        SystemDto system = objectMapper.readValue(result.getResponse().getContentAsString(), SystemDto.class);
+        assert(system.getName().equals(nameSystem));
+
     }
+
+    @Test
+    public void deleteSystem() throws Exception {
+        List<SystemEntity> systems = systemRepository.findAll();
+
+        mockMvc.perform(delete(SystemController.DELETE_SYSTEM.replace("{system_id}", systems.get(0).getId())))
+                .andExpect(status().isOk());
+    }
+
 }
